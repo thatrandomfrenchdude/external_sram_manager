280adbda0606fed50d5c47f29afae2dd05085a8f
Nicholas Debeurre <nicholas.debeurre@nxp.com>
2019-05-29 13:20:09 -0700

    updated defines

--- memory_manager.h
+++ memory_manager.h
@@ -9,11 +9,9 @@
 
 #define READ_COMMAND 0x03           //0000 0011
 #define WRITE_COMMAND 0x02          //0000 0010
-
 #define READ_MODE_DEFAULT 0x05      //0000 0101
 #define READ_MODE_PAGE 0x06         //0000 0110
 #define READ_MODE_BYTE 0x04         //0000 0100
-
 #define WRITE_MODE_DEFAULT 0x01     //0000 0001
 #define WRITE_MODE_PAGE 0x02        //0000 0010
 #define WRITE_MODE_BYTE 0x00        //0000 0000
